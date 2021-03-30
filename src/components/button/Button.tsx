36a6a7660ef09e043b35605a34f6e427831ebe0e
kazuhe <omc.ohara@gmail.com>
2021-03-31 02:04:17 +0900

    mod: Buttonコンポーネントにtype属性のpropsを追加

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -13,13 +13,18 @@ export type Props = {
   label: string
   wide?: boolean
   isEnabled?: boolean
+  type?: 'button' | 'submit' | 'reset'
   onClick?: () => void
 }
 
 /*
  * DOM
  */
-export const Button: React.FC<Props> = ({ isEnabled = true, ...props }) => (
+export const Button: React.FC<Props> = ({
+  isEnabled = true,
+  type = 'button',
+  ...props
+}) => (
   <>
     {props.link ? (
       <Link href={props.link} passHref>
@@ -27,6 +32,7 @@ export const Button: React.FC<Props> = ({ isEnabled = true, ...props }) => (
       </Link>
     ) : (
       <button
+        type={type}
         onClick={props.onClick}
         disabled={!isEnabled}
         className={[styles.btn, props.wide ? styles.isWide : ''].join(' ')}
